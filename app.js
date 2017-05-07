caef91a5e0b28ae0861e4c3423e3b2889bda61ed
forgetsunset <yangxiaosunset@gmail.com>
2017-05-08 00:55:19 +0800

    update slider

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-$(function() {      
-			$('.banner').unslider({
-				// speed: 300,               //  The speed to animate each slide (in milliseconds)
-				// delay: 5000,              //  The delay between slide animations (in milliseconds)
-				// // complete: function() {},  //  A function that gets called after every slide animation
-				// keys: false,               //  Enable keyboard (left, right) arrow shortcuts
-				// dots: false,               //  Display dot navigation
-				// fluid: false              //  Support responsive design. May break non-responsive designs
-				infinite: true
-			});
-
-			var unslider = $('.banner').unslider();
-
-		    $('.unslider-arrow').click(function() {
-		        var fn = this.className.split(' ')[1];
-		        //  Either do unslider.data('unslider').next() or .prev() depending on the className
-		        unslider.data('unslider')[fn]();
-		    });
+$('.fading-slider').unslider({
+	animation: 'fade', 
+	autoplay: true,
+	arrows: false,
+	nav: true,
+	delay: 5000
 });
